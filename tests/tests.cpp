4244e9ee63e9846c7a6b85c3328971b0499e88f2
Brick <6098371+0x1F9F1@users.noreply.github.com>
2019-02-11 13:02:34 +0000

    Wrapped test cases in CHECK_NOTHROW

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -66,32 +66,32 @@ void check_pattern(const mem::pattern& pattern, size_t original_size, size_t tri
 
 TEST_CASE("mem::pattern constructor")
 {
-    check_pattern(mem::pattern("01 02 03 04 05"), 5, 5, false, "\x01\x02\x03\x04\x05", "\xFF\xFF\xFF\xFF\xFF");
-    check_pattern(mem::pattern("01 02 03 04 ?"), 5, 4, false, "\x01\x02\x03\x04\x00", "\xFF\xFF\xFF\xFF\x00");
-    check_pattern(mem::pattern(" 01    02        03 04 05 "), 5, 5, false, "\x01\x02\x03\x04\x05", "\xFF\xFF\xFF\xFF\xFF");
-    check_pattern(mem::pattern("1 2 3 4 5"),      5, 5, false, "\x01\x02\x03\x04\x05", "\xFF\xFF\xFF\xFF\xFF");
-    check_pattern(mem::pattern("1 ?2 3 4? 5"),    5, 5, true,  "\x01\x02\x03\x40\x05", "\xFF\x0F\xFF\xF0\xFF");
-    check_pattern(mem::pattern("1? ? 3 ?? 5?"),   5, 5, true,  "\x10\x00\x03\x00\x50", "\xF0\x00\xFF\x00\xF0");
-    check_pattern(mem::pattern("?1 ? 3 ?? ?5"),   5, 5, true,  "\x01\x00\x03\x00\x05", "\x0F\x00\xFF\x00\x0F");
-    check_pattern(mem::pattern("01?12???34"),     5, 5, true,  "\x01\x01\x20\x00\x34", "\xFF\x0F\xF0\x00\xFF");
+    CHECK_NOTHROW(check_pattern(mem::pattern("01 02 03 04 05"), 5, 5, false, "\x01\x02\x03\x04\x05", "\xFF\xFF\xFF\xFF\xFF"));
+    CHECK_NOTHROW(check_pattern(mem::pattern("01 02 03 04 ?"), 5, 4, false, "\x01\x02\x03\x04\x00", "\xFF\xFF\xFF\xFF\x00"));
+    CHECK_NOTHROW(check_pattern(mem::pattern(" 01    02        03 04 05 "), 5, 5, false, "\x01\x02\x03\x04\x05", "\xFF\xFF\xFF\xFF\xFF"));
+    CHECK_NOTHROW(check_pattern(mem::pattern("1 2 3 4 5"),      5, 5, false, "\x01\x02\x03\x04\x05", "\xFF\xFF\xFF\xFF\xFF"));
+    CHECK_NOTHROW(check_pattern(mem::pattern("1 ?2 3 4? 5"),    5, 5, true,  "\x01\x02\x03\x40\x05", "\xFF\x0F\xFF\xF0\xFF"));
+    CHECK_NOTHROW(check_pattern(mem::pattern("1? ? 3 ?? 5?"),   5, 5, true,  "\x10\x00\x03\x00\x50", "\xF0\x00\xFF\x00\xF0"));
+    CHECK_NOTHROW(check_pattern(mem::pattern("?1 ? 3 ?? ?5"),   5, 5, true,  "\x01\x00\x03\x00\x05", "\x0F\x00\xFF\x00\x0F"));
+    CHECK_NOTHROW(check_pattern(mem::pattern("01?12???34"),     5, 5, true,  "\x01\x01\x20\x00\x34", "\xFF\x0F\xF0\x00\xFF"));
 
-    check_pattern(mem::pattern("01 02 03#3 04 05"),    7, 7, false, "\x01\x02\x03\x03\x03\x04\x05", "\xFF\xFF\xFF\xFF\xFF\xFF\xFF");
-    check_pattern(mem::pattern("01 02 03&F#3 04 05"),  7, 7, true,  "\x01\x02\x03\x03\x03\x04\x05", "\xFF\xFF\x0F\x0F\x0F\xFF\xFF");
-    check_pattern(mem::pattern("01 02 33&F0#3 04 05"), 7, 7, true,  "\x01\x02\x30\x30\x30\x04\x05", "\xFF\xFF\xF0\xF0\xF0\xFF\xFF");
+    CHECK_NOTHROW(check_pattern(mem::pattern("01 02 03#3 04 05"),    7, 7, false, "\x01\x02\x03\x03\x03\x04\x05", "\xFF\xFF\xFF\xFF\xFF\xFF\xFF"));
+    CHECK_NOTHROW(check_pattern(mem::pattern("01 02 03&F#3 04 05"),  7, 7, true,  "\x01\x02\x03\x03\x03\x04\x05", "\xFF\xFF\x0F\x0F\x0F\xFF\xFF"));
+    CHECK_NOTHROW(check_pattern(mem::pattern("01 02 33&F0#3 04 05"), 7, 7, true,  "\x01\x02\x30\x30\x30\x04\x05", "\xFF\xFF\xF0\xF0\xF0\xFF\xFF"));
 
-    check_pattern(mem::pattern("01 02 03&F"), 3, 3, true, "\x01\x02\x03", "\xFF\xFF\x0F");
-    check_pattern(mem::pattern("01 02 03#2"), 4, 4, false, "\x01\x02\x03\x03", "\xFF\xFF\xFF\xFF");
+    CHECK_NOTHROW(check_pattern(mem::pattern("01 02 03&F"), 3, 3, true, "\x01\x02\x03", "\xFF\xFF\x0F"));
+    CHECK_NOTHROW(check_pattern(mem::pattern("01 02 03#2"), 4, 4, false, "\x01\x02\x03\x03", "\xFF\xFF\xFF\xFF"));
 
-    check_pattern(mem::pattern("12345678"), 4, 4, false, "\x12\x34\x56\x78", "\xFF\xFF\xFF\xFF");
+    CHECK_NOTHROW(check_pattern(mem::pattern("12345678"), 4, 4, false, "\x12\x34\x56\x78", "\xFF\xFF\xFF\xFF"));
 
-    check_pattern(mem::pattern("? 01 02 03 04 ? ? ?"), 8, 5, true, "\x00\x01\x02\x03\x04\x00\x00\x00", "\x00\xFF\xFF\xFF\xFF\x00\x00\x00");
+    CHECK_NOTHROW(check_pattern(mem::pattern("? 01 02 03 04 ? ? ?"), 8, 5, true, "\x00\x01\x02\x03\x04\x00\x00\x00", "\x00\xFF\xFF\xFF\xFF\x00\x00\x00"));
 
-    check_pattern(mem::pattern("\x12\x34\x56\x78\xAB", "x?xx?"), 5, 4, true, "\x12\x00\x56\x78\x00", "\xFF\x00\xFF\xFF\x00");
+    CHECK_NOTHROW(check_pattern(mem::pattern("\x12\x34\x56\x78\xAB", "x?xx?"), 5, 4, true, "\x12\x00\x56\x78\x00", "\xFF\x00\xFF\xFF\x00"));
 
-    check_pattern(mem::pattern("Hello", nullptr), 5, 5, false, "\x48\x65\x6C\x6C\x6F", "\xFF\xFF\xFF\xFF\xFF");
+    CHECK_NOTHROW(check_pattern(mem::pattern("Hello", nullptr), 5, 5, false, "\x48\x65\x6C\x6C\x6F", "\xFF\xFF\xFF\xFF\xFF"));
 
-    check_pattern(mem::pattern("\x12\x34\x56\x78\xAB", "\xFF\x00\xFF\xFF\x00", 5), 5, 4, true, "\x12\x00\x56\x78\x00", "\xFF\x00\xFF\xFF\x00");
-    check_pattern(mem::pattern("\x12\x34\x56\x78\xAB", nullptr, 5), 5, 5, false, "\x12\x34\x56\x78\xAB", "\xFF\xFF\xFF\xFF\xFF");
+    CHECK_NOTHROW(check_pattern(mem::pattern("\x12\x34\x56\x78\xAB", "\xFF\x00\xFF\xFF\x00", 5), 5, 4, true, "\x12\x00\x56\x78\x00", "\xFF\x00\xFF\xFF\x00"));
+    CHECK_NOTHROW(check_pattern(mem::pattern("\x12\x34\x56\x78\xAB", nullptr, 5), 5, 5, false, "\x12\x34\x56\x78\xAB", "\xFF\xFF\xFF\xFF\xFF"));
 }
 
 void check_pattern_results(mem::region whole_region, const mem::pattern& pattern, const std::vector<uint8_t>& scan_data, const std::unordered_set<size_t>& offsets)
@@ -138,53 +138,53 @@ TEST_CASE("mem::pattern scan")
 
     mem::region scan_region(raw_data + page_size, raw_size - (2 * page_size));
 
-    check_pattern_results(scan_region, mem::pattern("01 02 03 04 05"), {
+    CHECK_NOTHROW(check_pattern_results(scan_region, mem::pattern("01 02 03 04 05"), {
         0x01, 0x02, 0x03, 0x04, 0x05
     },{
         0
-    });
+    }));
 
-    check_pattern_results(scan_region, mem::pattern("01 02 03 04 ?"), {
+    CHECK_NOTHROW(check_pattern_results(scan_region, mem::pattern("01 02 03 04 ?"), {
         0x01, 0x02, 0x03, 0x04, 0x05
     },{
         0
-    });
+    }));
 
-    check_pattern_results(scan_region, mem::pattern("01 02 03 04 ?"), {
+    CHECK_NOTHROW(check_pattern_results(scan_region, mem::pattern("01 02 03 04 ?"), {
         0x01, 0x02, 0x03, 0x04
     },{
 
-    });
+    }));
 
-    check_pattern_results(scan_region, mem::pattern("01 02 01 02 01"), {
+    CHECK_NOTHROW(check_pattern_results(scan_region, mem::pattern("01 02 01 02 01"), {
         0x01, 0x02, 0x01, 0x02, 0x01, 0x02, 0x01, 0x02, 0x01, 0x02, 0x01
     }, {
         0, 2, 4, 6
-    });
+    }));
 
-    check_pattern_results(scan_region, mem::pattern(""), {
+    CHECK_NOTHROW(check_pattern_results(scan_region, mem::pattern(""), {
 
     }, {
 
-    });
+    }));
 
-    check_pattern_results(scan_region, mem::pattern(""), {
+    CHECK_NOTHROW(check_pattern_results(scan_region, mem::pattern(""), {
         0x01, 0x02, 0x03, 0x04, 0x05, 0x06
     }, {
 
-    });
+    }));
 
-    check_pattern_results(scan_region, mem::pattern("01 ?2 3? 45"), {
+    CHECK_NOTHROW(check_pattern_results(scan_region, mem::pattern("01 ?2 3? 45"), {
         0x02, 0x59, 0x72, 0x01, 0x01, 0x02, 0x34, 0x45, 0x59, 0x92
     }, {
         4
-    });
+    }));
 
-    check_pattern_results(scan_region, mem::pattern("01 ?2 3? 45"), {
+    CHECK_NOTHROW(check_pattern_results(scan_region, mem::pattern("01 ?2 3? 45"), {
         0x02, 0x59, 0x72, 0x01, 0x01, 0x02, 0x43, 0x45, 0x59, 0x92
     }, {
 
-    });
+    }));
 
     mem::protect_free(raw_data, raw_size);
 }
@@ -220,12 +220,12 @@ void check_pointer_aligment(mem::pointer address, size_t alignment, mem::pointer
 
 TEST_CASE("mem::pointer align")
 {
-    check_pointer_aligment(13, 1, 13, 13);
-    check_pointer_aligment(13, 2, 12, 14);
-    check_pointer_aligment(13, 5, 10, 15);
-    check_pointer_aligment(16, 5, 15, 20);
-    check_pointer_aligment(14, 5, 10, 15);
-    check_pointer_aligment(15, 15, 15, 15);
+    CHECK_NOTHROW(check_pointer_aligment(13, 1, 13, 13));
+    CHECK_NOTHROW(check_pointer_aligment(13, 2, 12, 14));
+    CHECK_NOTHROW(check_pointer_aligment(13, 5, 10, 15));
+    CHECK_NOTHROW(check_pointer_aligment(16, 5, 15, 20));
+    CHECK_NOTHROW(check_pointer_aligment(14, 5, 10, 15));
+    CHECK_NOTHROW(check_pointer_aligment(15, 15, 15, 15));
 }
 
 template <typename T>
@@ -238,9 +238,9 @@ void check_any_pointer()
 
 TEST_CASE("mem::pointer any")
 {
-    check_any_pointer<uintptr_t>();
-    check_any_pointer<int*>();
-    check_any_pointer<void*>();
+    CHECK_NOTHROW(check_any_pointer<uintptr_t>());
+    CHECK_NOTHROW(check_any_pointer<int*>());
+    CHECK_NOTHROW(check_any_pointer<void*>());
 }
 
 void check_hex_conversion(const void* data, size_t length, bool upper_case, bool padded, const char* expected)
@@ -250,10 +250,10 @@ void check_hex_conversion(const void* data, size_t length, bool upper_case, bool
 
 TEST_CASE("mem::as_hex")
 {
-    check_hex_conversion("\x01\x23\x45\x67\x89\xAB\xCD\xEF", 8, true,  true, "01 23 45 67 89 AB CD EF");
-    check_hex_conversion("\x01\x23\x45\x67\x89\xAB\xCD\xEF", 8, false, true, "01 23 45 67 89 ab cd ef");
-    check_hex_conversion("\x01\x23\x45\x67\x89\xAB\xCD\xEF", 8, true,  false, "0123456789ABCDEF");
-    check_hex_conversion("\x01\x23\x45\x67\x89\xAB\xCD\xEF", 8, false, false, "0123456789abcdef");
+    CHECK_NOTHROW(check_hex_conversion("\x01\x23\x45\x67\x89\xAB\xCD\xEF", 8, true,  true, "01 23 45 67 89 AB CD EF"));
+    CHECK_NOTHROW(check_hex_conversion("\x01\x23\x45\x67\x89\xAB\xCD\xEF", 8, false, true, "01 23 45 67 89 ab cd ef"));
+    CHECK_NOTHROW(check_hex_conversion("\x01\x23\x45\x67\x89\xAB\xCD\xEF", 8, true,  false, "0123456789ABCDEF"));
+    CHECK_NOTHROW(check_hex_conversion("\x01\x23\x45\x67\x89\xAB\xCD\xEF", 8, false, false, "0123456789abcdef"));
 }
 
 void check_unescape_string(const char* string, const void* data, size_t length, bool strict)
@@ -271,46 +271,43 @@ void check_unescape_string(const char* string, const void* data, size_t length, 
 
 TEST_CASE("mem::unescape")
 {
-    check_unescape_string(R"(\x12\x34)", "\x12\x34", 2, true);
-    check_unescape_string(R"(\0\1\10)", "\0\1\10", 3, true);
-    check_unescape_string(R"(\0\1\1011)", "\0\1\1011", 4, true);
-    check_unescape_string(R"(\1\2\3)", "\1\2\3", 3, true);
-    check_unescape_string(R"(Hello There)", "Hello There", 11, true);
-    check_unescape_string(R"(Hello\nThere)", "Hello\nThere", 11, true);
-    check_unescape_string(R"(Hello \"Bob)", "Hello \"Bob", 10, true);
+    CHECK_NOTHROW(check_unescape_string(R"(\x12\x34)", "\x12\x34", 2, true));
+    CHECK_NOTHROW(check_unescape_string(R"(\0\1\10)", "\0\1\10", 3, true));
+    CHECK_NOTHROW(check_unescape_string(R"(\0\1\1011)", "\0\1\1011", 4, true));
+    CHECK_NOTHROW(check_unescape_string(R"(\1\2\3)", "\1\2\3", 3, true));
+    CHECK_NOTHROW(check_unescape_string(R"(Hello There)", "Hello There", 11, true));
+    CHECK_NOTHROW(check_unescape_string(R"(Hello\nThere)", "Hello\nThere", 11, true));
+    CHECK_NOTHROW(check_unescape_string(R"(Hello \"Bob)", "Hello \"Bob", 10, true));
 
-    check_unescape_string(R"(I am a \U0001F9F1)", "I am a \xF0\x9F\xA7\xB1", 11, true);
+    CHECK_NOTHROW(check_unescape_string(R"(I am a \U0001F9F1)", "I am a \xF0\x9F\xA7\xB1", 11, true));
 
-    check_unescape_string(R"(\x123456!)", "V!", 2, false);
-    check_unescape_string(R"(\567ABC)", "wABC", 4, false);
-    check_unescape_string(R"(\xz)", "\0z", 2, false);
-    check_unescape_string(R"(\yz)", "yz", 2, false);
+    CHECK_NOTHROW(check_unescape_string(R"(\x123456!)", "V!", 2, false));
+    CHECK_NOTHROW(check_unescape_string(R"(\567ABC)", "wABC", 4, false));
+    CHECK_NOTHROW(check_unescape_string(R"(\xz)", "\0z", 2, false));
+    CHECK_NOTHROW(check_unescape_string(R"(\yz)", "yz", 2, false));
 
-    check_unescape_string(R"(\x123456)", "", 0, true);
-    check_unescape_string(R"(\567ABC)", "", 0, true);
-    check_unescape_string(R"(\xz)", "", 0, true);
-    check_unescape_string(R"(\yz)", "", 0, true);
+    CHECK_NOTHROW(check_unescape_string(R"(\x123456)", "", 0, true));
+    CHECK_NOTHROW(check_unescape_string(R"(\567ABC)", "", 0, true));
+    CHECK_NOTHROW(check_unescape_string(R"(\xz)", "", 0, true));
+    CHECK_NOTHROW(check_unescape_string(R"(\yz)", "", 0, true));
 }
 
 #if defined(_MSC_VER)
 # pragma warning(pop)
 #endif
 
+void check_prot_flags_roundtrip(mem::prot_flags flags)
+{
+    mem::prot_flags new_flags = mem::to_prot_flags(mem::from_prot_flags(flags));
+
+    REQUIRE(flags == new_flags);
+}
+
 TEST_CASE("mem::prot_flags")
 {
-    const mem::prot_flags FLAGS[ ] =
-    {
-        mem::prot_flags::NONE,
-        mem::prot_flags::R,
-        mem::prot_flags::RW,
-        mem::prot_flags::RX,
-        mem::prot_flags::RWX,
-    };
-
-    for (mem::prot_flags flags : FLAGS)
-    {
-        mem::prot_flags new_flags = mem::to_prot_flags(mem::from_prot_flags(flags));
-
-        REQUIRE(flags == new_flags);
-    }
+    CHECK_NOTHROW(check_prot_flags_roundtrip(mem::prot_flags::NONE));
+    CHECK_NOTHROW(check_prot_flags_roundtrip(mem::prot_flags::R));
+    CHECK_NOTHROW(check_prot_flags_roundtrip(mem::prot_flags::RW));
+    CHECK_NOTHROW(check_prot_flags_roundtrip(mem::prot_flags::RX));
+    CHECK_NOTHROW(check_prot_flags_roundtrip(mem::prot_flags::RWX));
 }
