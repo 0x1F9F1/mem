7cf621c2ae4d1131d75a143c56b9c5ea7160f48b
Brick <6098371+0x1F9F1@users.noreply.github.com>
2019-02-26 07:13:16 +0000

    mem::find_byte - Manually unrolled SIMD comparison loop

--- include/mem/simd_scanner.h
+++ include/mem/simd_scanner.h
@@ -210,52 +210,108 @@ namespace mem
 #        define l_SIMD_TYPE __m256i
 #        define l_SIMD_FILL(x) _mm256_set1_epi8(static_cast<char>(x))
 #        define l_SIMD_LOAD(x) _mm256_lddqu_si256(x)
-#        define l_SIMD_CMPEQ_MASK(x, y) _mm256_movemask_epi8(_mm256_cmpeq_epi8(x, y))
+#        define l_SIMD_CMPEQ(x, y) _mm256_cmpeq_epi8(x, y)
+#        define l_SIMD_MOVEMASK(x) _mm256_movemask_epi8(x)
 #    elif defined(MEM_SIMD_SSE3)
 #        define l_SIMD_TYPE __m128i
 #        define l_SIMD_FILL(x) _mm_set1_epi8(static_cast<char>(x))
 #        define l_SIMD_LOAD(x) _mm_lddqu_si128(x)
-#        define l_SIMD_CMPEQ_MASK(x, y) _mm_movemask_epi8(_mm_cmpeq_epi8(x, y))
+#        define l_SIMD_CMPEQ(x, y) _mm_cmpeq_epi8(x, y)
+#        define l_SIMD_MOVEMASK(x) _mm_movemask_epi8(x)
 #    elif defined(MEM_SIMD_SSE2)
 #        define l_SIMD_TYPE __m128i
 #        define l_SIMD_FILL(x) _mm_set1_epi8(static_cast<char>(x))
 #        define l_SIMD_LOAD(x) _mm_loadu_si128(x)
-#        define l_SIMD_CMPEQ_MASK(x, y) _mm_movemask_epi8(_mm_cmpeq_epi8(x, y))
+#        define l_SIMD_CMPEQ(x, y) _mm_cmpeq_epi8(x, y)
+#        define l_SIMD_MOVEMASK(x) _mm_movemask_epi8(x)
 #    else
 #        error Sorry, No Potatoes
 #    endif
 
-        if (MEM_LIKELY(num >= sizeof(l_SIMD_TYPE)))
+#    define l_SIMD_SIZEOF(N) (sizeof(l_SIMD_TYPE) * N)
+
+        if (MEM_LIKELY(num >= l_SIMD_SIZEOF(1)))
         {
             const l_SIMD_TYPE simd_value = l_SIMD_FILL(value);
 
-            do
+            while (MEM_LIKELY(num >= l_SIMD_SIZEOF(4)))
             {
-                const int mask = l_SIMD_CMPEQ_MASK(l_SIMD_LOAD(reinterpret_cast<const l_SIMD_TYPE*>(ptr)), simd_value);
+                num -= l_SIMD_SIZEOF(4);
+
+                const l_SIMD_TYPE value0 = l_SIMD_LOAD(reinterpret_cast<const l_SIMD_TYPE*>(ptr));
+                const l_SIMD_TYPE value1 = l_SIMD_LOAD(reinterpret_cast<const l_SIMD_TYPE*>(ptr + l_SIMD_SIZEOF(1)));
+                const l_SIMD_TYPE value2 = l_SIMD_LOAD(reinterpret_cast<const l_SIMD_TYPE*>(ptr + l_SIMD_SIZEOF(2)));
+                const l_SIMD_TYPE value3 = l_SIMD_LOAD(reinterpret_cast<const l_SIMD_TYPE*>(ptr + l_SIMD_SIZEOF(3)));
+
+                const l_SIMD_TYPE equal0 = l_SIMD_CMPEQ(value0, simd_value);
+                const l_SIMD_TYPE equal1 = l_SIMD_CMPEQ(value1, simd_value);
+                const l_SIMD_TYPE equal2 = l_SIMD_CMPEQ(value2, simd_value);
+                const l_SIMD_TYPE equal3 = l_SIMD_CMPEQ(value3, simd_value);
+
+                {
+                    const int mask = l_SIMD_MOVEMASK(equal0);
+
+                    if (MEM_UNLIKELY(mask != 0))
+                        return ptr + bsf(static_cast<unsigned int>(mask));
+                }
+
+                {
+                    const int mask = l_SIMD_MOVEMASK(equal1);
+
+                    if (MEM_UNLIKELY(mask != 0))
+                        return ptr + l_SIMD_SIZEOF(1) + bsf(static_cast<unsigned int>(mask));
+                }
+
+                {
+                    const int mask = l_SIMD_MOVEMASK(equal2);
+
+                    if (MEM_UNLIKELY(mask != 0))
+                        return ptr + l_SIMD_SIZEOF(2) + bsf(static_cast<unsigned int>(mask));
+                }
+
+                {
+                    const int mask = l_SIMD_MOVEMASK(equal3);
+
+                    if (MEM_UNLIKELY(mask != 0))
+                        return ptr + l_SIMD_SIZEOF(3) + bsf(static_cast<unsigned int>(mask));
+                }
+
+                ptr += l_SIMD_SIZEOF(4);
+            }
+
+            while (MEM_LIKELY(num >= l_SIMD_SIZEOF(1)))
+            {
+                num -= l_SIMD_SIZEOF(1);
+
+                const int mask =
+                    l_SIMD_MOVEMASK(l_SIMD_CMPEQ(l_SIMD_LOAD(reinterpret_cast<const l_SIMD_TYPE*>(ptr)), simd_value));
 
                 if (MEM_UNLIKELY(mask != 0))
                     return ptr + bsf(static_cast<unsigned int>(mask));
 
-                num -= sizeof(l_SIMD_TYPE);
-                ptr += sizeof(l_SIMD_TYPE);
-            } while (MEM_LIKELY(num >= sizeof(l_SIMD_TYPE)));
+                ptr += l_SIMD_SIZEOF(1);
+            }
         }
 
-        while (MEM_LIKELY(num != 0))
-        {
-            if (MEM_UNLIKELY(*ptr == value))
-                return ptr;
-
-            --num;
-            ++ptr;
-        }
-
-        return ptr;
+#    undef l_SIMD_SIZEOF
 
 #    undef l_SIMD_TYPE
 #    undef l_SIMD_FILL
 #    undef l_SIMD_LOAD
-#    undef l_SIMD_CMPEQ_MASK
+#    undef l_SIMD_CMPEQ
+#    undef l_SIMD_MOVEMASK
+
+        while (MEM_LIKELY(num != 0))
+        {
+            --num;
+
+            if (MEM_UNLIKELY(*ptr == value))
+                return ptr;
+
+            ++ptr;
+        }
+
+        return ptr;
 #else
         const byte* result = static_cast<const byte*>(std::memchr(ptr, value, num));
 
